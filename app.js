ae54376db85387af28e46921ac32ffeed413c1f7
jeeva-diptiranjan <ranjandipti@greenjeeva.com>
2021-09-25 22:51:08 +0530

    changes in message

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use('/api/users',userRouter);
 
 app.listen(process.env.APP_PORT,()=>{
-    console.log('My server is running on PORT:', process.env.APP_PORT);
+    console.log('My server is running on PORT :', process.env.APP_PORT);
 })
